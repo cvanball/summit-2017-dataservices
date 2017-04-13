eacc11d4e5073b922be71b241985ea12a5fc73e1
Cojan van Ballegooijen <cvanball@redhat.com>
2017-04-13 21:14:38 +0200

    Move popuate db hook to postgresql Deployment Config

--- labs/lab3/projects/food-service/deployments/deployments/sql/initdb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -x
-
-echo "PostgreSQL Database Initialization"
-PGPASSWORD=$DATABASE_PASSWORD /usr/bin/psql -h $WINEAPP_POSTGRESQL_SERVICE_HOST -d $DATABASE_NAME -U $DATABASE_USER -f $HOME/source/deployments/sql/initdb-psql.sql
