7fefce9920180ccf06246896a70ea4d5f4b9bc55
Cojan van Ballegooijen <cvanball@redhat.com>
2017-04-13 16:15:42 +0200

    Updated populate psql script

--- labs/lab4_ocp/extensions/sql/initdb.sh
+++ labs/lab4_ocp/extensions/sql/initdb.sh
@@ -3,4 +3,4 @@
 set -x
 
 echo "PostgreSQL Database Initialization"
-PGPASSWORD=$POSTGRESQL_PASSWORD /usr/bin/psql -h ${RHAPP_POSTGRESQL_SERVICE_HOST} -d ${POSTGRESQL_DATABASE} -U $POSTGRESQL_USERNAME -f ${HOME}/source/extensions/extras/sql/initdb-psql.sql
+PGPASSWORD=$POSTGRESQL_PASSWORD /usr/bin/psql -h $RHAPP_POSTGRESQL_SERVICE_HOST -d $POSTGRESQL_DATABASE -U $POSTGRESQL_USERNAME -f $HOME/source/extensions/extras/sql/initdb-psql.sql
