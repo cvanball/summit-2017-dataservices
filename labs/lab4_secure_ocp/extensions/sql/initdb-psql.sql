481f598b4292cb1fadf7ce52801d8ccc4e3f9843
Madou <mcouliba@redhat.com>
2017-04-14 20:34:33 +0200

    Fix 'add new entries' for lab4_secure_ocp

--- labs/lab4_secure_ocp/extensions/sql/initdb-psql.sql
+++ labs/lab4_secure_ocp/extensions/sql/initdb-psql.sql
@@ -6,11 +6,11 @@ DROP TABLE IF EXISTS food;
 --------------------
 
 CREATE TABLE wine (
-wine_id integer NOT NULL,
+wine_id varchar(255) NOT NULL,
 vineyard varchar(255) NOT NULL,
 varietal varchar(255) NOT NULL,
 name varchar(255) NOT NULL,
-year integer NOT NULL,
+vintageyear integer NOT NULL,
 country varchar(255) NOT NULL,
  PRIMARY KEY (wine_id)
 );
@@ -21,7 +21,7 @@ country varchar(255) NOT NULL,
 --------------------
 
 CREATE TABLE food (
- r_id integer NOT NULL,
+ r_id varchar(255) NOT NULL,
  author varchar(255) NOT NULL,
  category varchar(255) NOT NULL,
  name varchar(255) NOT NULL,
@@ -33,20 +33,20 @@ CREATE TABLE food (
 ---------------------
 -- WINE TABLE DATA --
 ---------------------
-INSERT INTO wine(wine_id, vineyard, varietal, name, year, country)
-VALUES (1,'Bordeaux', 'Summit2017', 'Bordeaux', 2011,'France');
-INSERT INTO wine(wine_id, vineyard, varietal, name, year, country)
-VALUES (2,'Bernardus', 'Summit2017', 'Bernardus', 2014,'USA');
-INSERT INTO wine(wine_id, vineyard, varietal, name, year, country)
-VALUES (3,'Aldenborgh', 'Summit2017', 'Aldenborgh Eyra', 2017,'Netherlands');
+INSERT INTO wine(wine_id, vineyard, varietal, name, vintageyear, country)
+VALUES ('1','Bordeaux', 'Summit2017', 'Bordeaux', 2011,'France');
+INSERT INTO wine(wine_id, vineyard, varietal, name, vintageyear, country)
+VALUES ('2','Bernardus', 'Summit2017', 'Bernardus', 2014,'USA');
+INSERT INTO wine(wine_id, vineyard, varietal, name, vintageyear, country)
+VALUES ('3','Aldenborgh', 'Summit2017', 'Aldenborgh Eyra', 2017,'Netherlands');
 
 ---------------------
 -- FOOD TABLE DATA --
 ---------------------
 INSERT INTO food(r_id, author, category, name, rating, country)
-VALUES (1,'Bakery', 'Bread', 'Baguette', 2017,'France');
+VALUES ('1','Bakery', 'Bread', 'Baguette', 2017,'France');
 INSERT INTO food(r_id, author, category, name, rating, country)
-VALUES (2,'Mc Donalds', 'Fast Food', 'Big Mac', 1977,'USA');
+VALUES ('2','Mc Donalds', 'Fast Food', 'Big Mac', 1977,'USA');
 INSERT INTO food(r_id, author, category, name, rating, country)
-VALUES (3,'Goudse', 'Agricultural', 'Cheese', 1184,'Netherlands');
+VALUES ('3','Goudse', 'Agricultural', 'Cheese', 1184,'Netherlands');
 
