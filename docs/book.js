eed2e48ed7f090fd480f6c48b3a9c6bdd251e5de
Cojan van Ballegooijen <cvanball@redhat.com>
2017-04-27 13:41:12 +0200

    Added book.js

---
+++ docs/book.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // Documentation for GitBook is stored under "docs"
+    root: './docs',
+    title: 'L140372 - Exposing data as services in a Microservices Architecture on OpenShift'
+};
